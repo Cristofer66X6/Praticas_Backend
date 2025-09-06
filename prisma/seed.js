ad34b697d512366a8b2ba7f5bbcadb8dd86225ae
Cristofer66X6 <alberto.c666c@gmail.com>
2025-09-06 11:40:32 -0600

    Corregi express

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,12 +2,22 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.pedido.deleteMany();
+  await prisma.cliente.deleteMany();
   const cliente1 = await prisma.cliente.create({
     data: { nombre: "Andrés Pérez", email: "andres@example.com" },
   });
 
   const cliente2 = await prisma.cliente.create({
-    data: { nombre: "María López", email: "maria@example.com" },
+    data: { nombre: "María López", email: "maria@exmple.com" },
+  });
+
+  const cliente3 = await prisma.cliente.create({
+    data: { nombre: "Miguel Uriel", email: "miguel@xample.com" },
+  });
+
+  const cliente6 = await prisma.cliente.create({
+    data: { nombre: "Andres Manuel", email: "anuel89@ample.com" },
   });
 
   await prisma.pedido.createMany({
@@ -15,11 +25,14 @@ async function main() {
       { clienteId: cliente1.id, estado: "pendiente", total: 500 },
       { clienteId: cliente1.id, estado: "enviado", total: 1200 },
       { clienteId: cliente2.id, estado: "pendiente", total: 750 },
+      { clienteId: cliente3.id, estado: "pendiente", total: 750 },
+      { clienteId: cliente6.id, estado: "pendiente", total: 750 },
     ],
   });
 }
 
 main()
+
   .then(() => console.log("✅ Seeds insertados"))
   .catch((e) => console.error(e))
   .finally(async () => await prisma.$disconnect());
